55f9f86e4b2a100d306099f5f6a3b489d15b2df6
Пригарин Олег Юрьевич <lolomap07@yandex.ru>
2026-05-04 17:13:05 +0300

    Topics keywords

---
+++ Assets/Scripts/Data/TopicSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Data
+{
+    [CreateAssetMenu(fileName = "Topic", menuName = "Tasks/Topic", order = -1000)]
+    public class TopicSO : ScriptableObject
+    {
+        public string Name;
+        public List<string> Keywords;
+    }
+}
---
+++ Assets/Scripts/Data/TopicsDB.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Data
+{
+    [CreateAssetMenu(fileName = "TopicsDB", menuName = "Tasks/TopicsDB", order = -1000)]
+    public class TopicsDB : ScriptableObject
+    {
+        public List<TopicSO> Topics;
+    }
+}
